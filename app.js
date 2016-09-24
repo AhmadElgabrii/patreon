31f7519c75d6df120a211f6245934eab1bbfe862
Ahmad Elgabri <ahmedlg46@gmail.com>
2016-09-25 01:54:04 +0200

    some code modified

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var app = angular.module('patreon', []);
-app.controller('bodyController',function($scope){
-  console.log($scope);
+app.controller('bodyController',function($scope,$http){
+  $http.get('db.json')
+       .then(function(res){
+          $scope.db = res.data;
+          console.log($scope.db);
+        });
+        console.log("This is the line below the http request");
 });
